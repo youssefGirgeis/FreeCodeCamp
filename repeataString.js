10cfa8a90d4a41b069c6f51a1c34ec5e832d1ed0
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-10-16 22:50:35 -0400

    add description

--- repeataString.js
+++ repeataString.js
@@ -1,3 +1,8 @@
+/*
+Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+*/
+
 
 
 function repeatStringNumTimes(str, num) {
