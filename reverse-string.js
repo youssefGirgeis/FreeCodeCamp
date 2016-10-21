6841b726e54d763f8657b19f16c03c53da727f8b
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-10-20 23:15:15 -0400

    problem description

--- reverse-string.js
+++ reverse-string.js
@@ -1,3 +1,11 @@
+/*
+Reverse the provided string.
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+*/
+
 function reverseString(str) {
   var arr1 = str.split('');
   var arr2 = arr1.reverse();
